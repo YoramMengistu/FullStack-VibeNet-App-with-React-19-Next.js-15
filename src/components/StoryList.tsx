f63932ffd663c65fcd2903ba3c6d15a113c72b0d
YoramMengistu <119293009+YoramMengistu@users.noreply.github.com>
2024-08-08 13:28:15 +0300

    test15

--- src/components/StoryList.tsx
+++ src/components/StoryList.tsx
@@ -2,9 +2,7 @@
 
 import { addStory } from "@/lib/actions";
 import { useUser } from "@clerk/nextjs";
-// import { User } from "@prisma/client";
-import type { Story, User } from "@prisma/client";
-
+import { Story, User } from "@prisma/client";
 import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
 import { useOptimistic, useState } from "react";
